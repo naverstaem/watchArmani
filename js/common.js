12e3bb227e0e742812e337880e18d56f548dd32f
fanian <rfanian@gmail.com>
2014-09-25 11:26:27 +0400

    add other blocks

--- js/common.js
+++ js/common.js
@@ -3,15 +3,15 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
-//    $(window).scroll(function () {
-//        if ( $(this).scrollTop() > 200 && !$('.scrollMenu').hasClass('open') ) {
-//            $('.scrollMenu').addClass('open');
-//            $('.scrollMenu').slideDown();
-//        } else if ( $(this).scrollTop() <= 200 ) {
-//            $('.scrollMenu').removeClass('open');
-//            $('.scrollMenu').slideUp();
-//        }
-//    });
+    $(window).scroll(function () {
+        if ( $(this).scrollTop() > 200 && !$('.scrollMenu').hasClass('open') ) {
+            $('.scrollMenu').addClass('open');
+            $('.scrollMenu').slideDown();
+        } else if ( $(this).scrollTop() <= 200 ) {
+            $('.scrollMenu').removeClass('open');
+            $('.scrollMenu').slideUp();
+        }
+    });
 //    jQuery(document).ready(function($) {
 //        var nav = $('.menu');
 //
